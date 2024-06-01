8bda239b2546f8fecf95a5c23ee46d335f7e7ab8
Julian Paule <nicer.typ@gmail.com>
2024-06-01 18:54:31 +0200

    working version

--- html5game/tph_multitouch.js
+++ html5game/tph_multitouch.js
@@ -1,23 +1,7 @@
-var touchXCoords = [0, 0, 0, 0];
 function getTouchXCoordinates() {
-    return JSON.stringify(touchXCoords);
-}
-
-function setTouchXCoordinates(event) {
-    touchXCoords = [0, 0, 0, 0];
-    for (let i = 0; i < Math.min(event.touches.length, 4); i++) {
-        touchXCoords[i] = event.touches[i].clientX;
+    let xCoordinates = [];
+    for (let i = 0; i < touches.length; i++) {
+        xCoordinates.push(touches[i].x);
     }
-
-    // Return the array
-    return touchXCoords;
+    return JSON.stringify(xCoordinates);
 }
-
-// Example usage of the function with touchstart and touchmove event listeners
-document.addEventListener('touchstart', function(event) {
-    setTouchXCoordinates(event);
-});
-
-document.addEventListener('touchmove', function(event) {
-    setTouchXCoordinates(event);
-});
